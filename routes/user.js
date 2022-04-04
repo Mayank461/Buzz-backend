03ac93c94f51aa932934f101bae57e2c1a7ad92b
mahir-ttn <mahir.asrani@tothenew.com>
2022-04-04 16:33:43 +0530

    confirm friend & delete req route

--- routes/user.js
+++ routes/user.js
@@ -22,7 +22,54 @@ router.post('/sendRequest/:id', async (req, res) => {
     await friendUser.save();
     await myUser.save();
 
+    res.send('friend request sent');
+  } catch (error) {
+    res.sendStatus(400);
+    console.log(error.message);
+  }
+});
+
+router.post('/confirmRequest/:id', async (req, res) => {
+  try {
+    const myUser = await userModel.findById(req.user.id);
+    const friendUser = await userModel.findById(req.params.id);
+
+    if (myUser.friends.myFriends.includes(req.params.id))
+      return res.status(400).send('Already in your Friend list');
+
+    //   remove from requests array
+    myUser.friends.mySentRequests.pull(friendUser._id);
+    friendUser.friends.myFriendRequests.pull(myUser._id);
+    //   add to friend array
+    myUser.friends.myFriends.push(friendUser._id);
+    friendUser.friends.myFriends.push(myUser._id);
+
+    await friendUser.save();
+    await myUser.save();
+
     res.send('friend added');
+  } catch (error) {
+    res.sendStatus(400);
+    console.log(error.message);
+  }
+});
+
+router.post('/deleteRequest/:id', async (req, res) => {
+  try {
+    const myUser = await userModel.findById(req.user.id);
+    const friendUser = await userModel.findById(req.params.id);
+
+    if (myUser.friends.myFriends.includes(req.params.id))
+      return res.status(400).send('Already in your Friend list');
+
+    //   remove from requests array
+    myUser.friends.mySentRequests.pull(friendUser._id);
+    friendUser.friends.myFriendRequests.pull(myUser._id);
+
+    await friendUser.save();
+    await myUser.save();
+
+    res.send('friend request cancelled');
   } catch (error) {
     res.sendStatus(400);
     console.log(error.message);
