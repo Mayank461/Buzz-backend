c4b11cab9a4439adb69d5d87bf8ba84742576fe8
Mayank461 <mayank.jaggi@tothenew.com>
2022-04-06 19:11:41 +0530

    Updated in userPost dynamic

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const authRouter = require('./auth');
 const userRouter = require('./user');
 const updateUser = require('./update');
 const loginUser = require('./loginUser');
+const userpost = require('./userPost');
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/update', updateUser);
+router.use('/userPost',userpost);
 
 module.exports = router;
