b1e77d7e23c031a211efa6d263a63277c0a144bb
kumarprashant24 <kumar.prashant@tothenew.com>
2022-04-08 00:52:19 +0530

    changes in main route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const authRouter = require('./auth');
 const userRouter = require('./user');
 const updateUser = require('./update');
 const loginUser = require('./loginUser');
+const userPost = require('./userPost');
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/update', updateUser);
+router.use('/userPost', userPost);
 
 module.exports = router;
