be2f7523777fba84b728b8689da3a4509788c514
mahir-ttn <mahir.asrani@tothenew.com>
2022-04-11 13:16:08 +0530

    loadmore button added

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,37 +1,45 @@
 const post = require('../services/post.service');
 
-
 module.exports.updatePost = async (req, res) => {
-  const { pic_url, caption, user_id } = req.body;  
+  const { pic_url, caption, user_id } = req.body;
   const result = await post.updatePost(user_id, pic_url, caption);
   res.sendStatus(result.status);
 };
 
-module.exports.getPost = async (req, res) => {  
-  const result = await post.getPost([req.user.id,...req.user.friends.myFriends]);
-  res.send(result);
+module.exports.getPost = async (req, res) => {
+  let page = parseInt(req.query.page) || 0;
+  let limit = parseInt(req.query.limit) || 10;
 
+  const result = await post.getPost(
+    [req.user.id, ...req.user.friends.myFriends],
+    page,
+    limit
+  );
+  res.send(result);
 };
 
-module.exports.like = async (req, res) => {  
-  const result = await post.inclike(req.body.post_id,req.user.id);
+module.exports.like = async (req, res) => {
+  const result = await post.inclike(req.body.post_id, req.user.id);
   res.sendStatus(result.status);
- };
+};
 
-
-module.exports.unlike = async (req,res) => {
-  const result = await post.dislike(req.body.post_id,req.user.id);
+module.exports.unlike = async (req, res) => {
+  const result = await post.dislike(req.body.post_id, req.user.id);
   res.sendStatus(result.status);
-}
+};
 
-module.exports.comment = async (req,res) => {
-  const result = await post.comment(req.body.post_id,req.body.comment.message,req.user.id,req.user.picture_url);
+module.exports.comment = async (req, res) => {
+  const result = await post.comment(
+    req.body.post_id,
+    req.body.comment.message,
+    req.user.id,
+    req.user.picture_url
+  );
   res.sendStatus(result.status);
   // console.log(req.body);
-}
-module.exports.report = async (req,res) => {
-  const result = await post.report(req.body.post_id,req.user.id);
+};
+module.exports.report = async (req, res) => {
+  const result = await post.report(req.body.post_id, req.user.id);
   res.sendStatus(result.status);
-//  console.log(req.body);
-}
-
+  //  console.log(req.body);
+};
--- services/post.service.js
+++ services/post.service.js
@@ -2,88 +2,88 @@ let post = require('../models/post.model');
 
 module.exports.updatePost = async (id, pic_url, caption) => {
   try {
-    // let newpost = newpost.  
+    // let newpost = newpost.
     await post({
       posted_by: id,
       post_url: pic_url,
-      post_caption : caption
+      post_caption: caption,
     }).save();
 
     return { status: 200 };
   } catch (error) {
     return { status: 400, message: error.message };
-
   }
 };
-module.exports.getPost = async (ids) => {
+module.exports.getPost = async (ids, page, limit) => {
   try {
-    // let newpost = newpost.  
-   return await post.find({
-    posted_by: {
-        $in: ids
-      }
-    }).populate({
-      path: "posted_by"
-    });    
+    return await post
+      .find({
+        posted_by: {
+          $in: ids,
+        },
+      })
+      .populate({
+        path: 'posted_by',
+      })
+      .sort({
+        _id: -1,
+      })
+      .skip(page * limit)
+      .limit(limit);
   } catch (error) {
     return { status: 400, message: error.message };
-
   }
 };
 
-
-module.exports.inclike = async (id,user_id) => {
+module.exports.inclike = async (id, user_id) => {
   try {
-    const mypost =await post.findById(id)
-    mypost.like.includes(user_id)?
-    mypost.like.pull(user_id):
-    mypost.like.push(user_id)
-    await mypost.save();
-  
-  return {status:200}
-  }catch(error) {
-    console.log(error);
-  }
-}
-
-module.exports.dislike = async (id,user_id) => {
-  try {
-    const mypost =await post.findById(id)
-    mypost.dislike.includes(user_id)?
-    mypost.dislike.pull(user_id):
-    mypost.dislike.push(user_id)
-    await mypost.save();
-  
-  return {status:200}
-  }catch(error) {
-    console.log(error);
-  }
-}
-
-module.exports.comment = async (id,message,user_id,picture_url) => {
-  try {
-    const mypost =await post.findById(id)
-    mypost.comment.push({user_id,message,picture_url})
+    const mypost = await post.findById(id);
+    mypost.like.includes(user_id)
+      ? mypost.like.pull(user_id)
+      : mypost.like.push(user_id);
     await mypost.save();
 
-
-  
-  return {status:200}
-  }catch(error) {
+    return { status: 200 };
+  } catch (error) {
     console.log(error);
   }
-}
-module.exports.report = async (id,user_id) => {
+};
+
+module.exports.dislike = async (id, user_id) => {
+  try {
+    const mypost = await post.findById(id);
+    mypost.dislike.includes(user_id)
+      ? mypost.dislike.pull(user_id)
+      : mypost.dislike.push(user_id);
+    await mypost.save();
+
+    return { status: 200 };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports.comment = async (id, message, user_id, picture_url) => {
+  try {
+    const mypost = await post.findById(id);
+    mypost.comment.push({ user_id, message, picture_url });
+    await mypost.save();
+
+    return { status: 200 };
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.report = async (id, user_id) => {
   try {
     // console.log(id);
     // console.log(user_id);
-    const mypost =await post.findById(id)
-    mypost.report.push(user_id)
+    const mypost = await post.findById(id);
+    mypost.report.push(user_id);
     await mypost.save();
 
-  
-  return {status:200}
-  }catch(error) {
+    return { status: 200 };
+  } catch (error) {
     console.log(error);
   }
-}
+};
