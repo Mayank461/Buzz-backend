8b82efb0967e6c9bcaecf9a6aaa90cd1c09332db
mahir-ttn <mahir.asrani@tothenew.com>
2022-04-20 19:25:57 +0530

    like dislike conflict fix

--- services/post.service.js
+++ services/post.service.js
@@ -1,31 +1,23 @@
-
 let user = require('../models/user.model');
 let post = require('../models/post.model');
 
 module.exports.allPost = async () => {
   try {
-
     return await post.find();
-   
   } catch (error) {
     return { status: 400, message: error.message };
-
   }
 };
 
 module.exports.delReport = async (id) => {
- 
   try {
-    // let newpost = newpost.  
+    // let newpost = newpost.
 
-     await post.deleteOne({_id:id});
-    
-   
+    await post.deleteOne({ _id: id });
 
     return { status: 200 };
   } catch (error) {
     return { status: 400, message: error.message };
-
   }
 };
 
@@ -63,47 +55,49 @@ module.exports.getPost = async (ids, page, limit) => {
     return { status: 400, message: error.message };
   }
 };
-module.exports.changeprofile = async (user_id,pic_url) => {
+module.exports.changeprofile = async (user_id, pic_url) => {
   try {
-    // let newpost = newpost.  
-    await user.findByIdAndUpdate(user_id,{
-     picture_url: pic_url
-   });
-  //  console.log(pic);
+    // let newpost = newpost.
+    await user.findByIdAndUpdate(user_id, {
+      picture_url: pic_url,
+    });
+    //  console.log(pic);
 
     return { status: 200 };
   } catch (error) {
     return { status: 400, message: error.message };
-
   }
 };
 module.exports.getchangeprofile = async (ids) => {
   try {
-    //let newpost = newpost.  
-   return await user.find({
-    posted_by: {
-        $in: ids
-      }
-    }).populate({
-      path: "posted_by"
-    }); 
-  // return await user.findById(ids)
-  // console.log(ids);   
+    //let newpost = newpost.
+    return await user
+      .find({
+        posted_by: {
+          $in: ids,
+        },
+      })
+      .populate({
+        path: 'posted_by',
+      });
+    // return await user.findById(ids)
+    // console.log(ids);
   } catch (error) {
     return { status: 400, message: error.message };
-
   }
 };
-
 
 module.exports.inclike = async (id, user_id) => {
   try {
     const mypost = await post.findById(id).populate({
       path: 'posted_by',
     });
+
+    mypost.dislike.includes(user_id) && mypost.dislike.pull(user_id);
     mypost.like.includes(user_id)
       ? mypost.like.pull(user_id)
       : mypost.like.push(user_id);
+
     await mypost.save();
     return mypost;
   } catch (error) {
@@ -116,9 +110,13 @@ module.exports.dislike = async (id, user_id) => {
     const mypost = await post.findById(id).populate({
       path: 'posted_by',
     });
+
+    mypost.like.includes(user_id) && mypost.like.pull(user_id);
+
     mypost.dislike.includes(user_id)
       ? mypost.dislike.pull(user_id)
       : mypost.dislike.push(user_id);
+
     await mypost.save();
 
     return mypost;
@@ -147,8 +145,7 @@ module.exports.report = async (id, user_id) => {
     const mypost = await post.findById(id).populate({
       path: 'posted_by',
     });
-    mypost.report.includes(user_id)?"":
-    mypost.report.push(user_id);
+    mypost.report.includes(user_id) ? '' : mypost.report.push(user_id);
     await mypost.save();
 
     return { status: 200 };
