12790c51ffcb1ddf136b6e9b32f900bd68776601
kumarprashant24 <kumar.prashant@tothenew.com>
2022-04-29 01:44:31 +0530

    removing default api

--- server.js
+++ server.js
@@ -25,9 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/',(req,res)=>{
-  res.json("server start")
-})
+
 app.use('/api', require('./routes/index'));
 
 mongoose.connect(MONGO_URI, (err) => {
